d5469d28c31855afff94d612c20e1e31d3d6f42a
Naomi Amuzie <ncaa2018@mymail.pomona.edu>
2020-10-27 23:39:11 -0500

    started file and added login screen with constraints. Connected to Backend with Parse

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "nzCxkQowzTq1pdBGodItNohJb6tf0Y3JixyQhaVA" // <- UPDATE with App ID
+                $0.clientKey = "3f6R52tT7kMVpgWNIPFoBPkml48lTypjzvxvvjxC" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by loan on 10/27/20.
+//  Copyright © 2020 naomia2022@hotmail.com. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success,error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil) //go to feed
+            }
+            else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+    }
+    @IBAction func onSignIn(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
