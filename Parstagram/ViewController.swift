72b70a8d5c240c277ce89dc3cec70430fd28ebe1
Naomi Amuzie <ncaa2018@mymail.pomona.edu>
2020-10-27 22:11:03 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by loan on 10/27/20.
+//  Copyright © 2020 naomia2022@hotmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
